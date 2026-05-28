a7ff6d81d03e20c07e7f96864b719385ccbba112
jobanana <shu6690@gmail.com>
2026-05-28 22:57:21 +0800

    部屬到render上進行使用

--- src/API/server.js
+++ src/API/server.js
@@ -131,5 +131,7 @@ const requestListener = (req, res) => {
 };
 
 const server = http.createServer(requestListener);
-
-server.listen(8080);
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
